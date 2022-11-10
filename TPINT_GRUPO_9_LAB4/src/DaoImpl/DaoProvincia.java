20e57f2375ced373317190b422df85e2fd5ae9c9
Federico Pereyra <crearcuen15@gmail.com>
2022-11-10 16:30:45 -0300

    Dao Provincia

--- TPINT_GRUPO_9_LAB4/src/DaoImpl/DaoProvincia.java
+++ TPINT_GRUPO_9_LAB4/src/DaoImpl/DaoProvincia.java
@@ -3,16 +3,47 @@ package DaoImpl;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 import Dao.IDaoProvincia;
 import Entidad.Provincia;
 
 public class DaoProvincia implements IDaoProvincia {
 
-	private static final String readall = "SELECT * FROM provincias WHERE ID = ?";
+	private static final String readall = "SELECT * FROM  provincias";
+	private static final String read = "SELECT * FROM provincias WHERE ID = ?";
+	
+
+	public ArrayList<Provincia> ReadAll()
+	{
+		PreparedStatement statement;
+		ResultSet resultSet;
+		ArrayList<Provincia> Provincias = new ArrayList<Provincia>();
+		Conexion conexion = Conexion.getConexion();
+		try 
+		{
+			statement = conexion.getSQLConexion().prepareStatement(readall);
+			resultSet = statement.executeQuery();
+			while(resultSet.next())
+			{
+				Provincias.add(getProvincias(resultSet));
+			}
+			
+			conexion.cerrarConexion();
+			
+		} 
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return Provincias;
+		
+		
+		
+		
+	}
 	
 	@Override
-	public Provincia readall(int ID) {
+ 	public Provincia readall(int ID) {
 		PreparedStatement statement;
 		ResultSet resultSet;
 		Conexion conexion = Conexion.getConexion();
@@ -35,5 +66,21 @@ public class DaoProvincia implements IDaoProvincia {
 		}
 		return provincia;
 	}
+	
+	
+	
+	
+	private Provincia getProvincias(ResultSet resultSet) throws SQLException
+	{
+		int id = resultSet.getInt("ID");
+		String descripcion = resultSet.getString("Descripcion");
+		
+		return new Provincia(id,descripcion);
+	}
+
+	
+	
+	
+	
 
 }
