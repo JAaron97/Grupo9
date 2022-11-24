e2fb703cb64e41e9ce0a23d1c1ba09752c63c7f4
Federico Pereyra <crearcuen15@gmail.com>
2022-11-24 00:07:20 -0300

    ValidacionTelefono

--- TPINT_GRUPO_9_LAB4/src/NegocioImpl/TelefonosNegImpl.java
+++ TPINT_GRUPO_9_LAB4/src/NegocioImpl/TelefonosNegImpl.java
@@ -9,18 +9,56 @@ public class TelefonosNegImpl implements TelefonosNeg{
 	DaoTelefono dTel = new DaoTelefono();
 	
 	@Override
-	public boolean Insert(Telefono telefono_add) {
-		return dTel.Insert(telefono_add);
+	public boolean Insert(Telefono telefono_add) 
+	{
+		if(ValidationTelefono(telefono_add)== true) 
+		{
+			return dTel.Insert(telefono_add);	
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	@Override
-	public boolean Update(Telefono telefono_update) {
-		return dTel.Update(telefono_update);
+	public boolean Update(Telefono telefono_update)
+	{
+		if(ValidationTelefono(telefono_update)== true) 
+		{
+			return dTel.Update(telefono_update);
+		}
+		else
+		{
+			return false;
+		}
 	}
 
 	@Override
-	public Telefono Read(int ID) {
+	public Telefono Read(int ID) 
+	{
 		return dTel.Read(ID);
 	}
+	
+	
+	private boolean ValidationTelefono(Telefono telefono)
+	{
+		try 
+		{
+			Integer.parseInt(telefono.getTelefono_1());
+		}
+		catch (Exception e) 
+		{
+			return false;
+		}
+		
+		if(telefono.getTelefono_1().length() != 8 || telefono.getTelefono_1().length() != 10) {
+			
+			return false;
+		}
+			
+			return true;	
+	}
+	
 
 }
